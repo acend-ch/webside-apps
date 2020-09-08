2117ad3a881829dadd965808c70b30e9d71fa5b6
Thomas Philipona <philipona@puzzle.ch>
2020-09-08 10:50:10 +0200

    Make port configurable

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
     "log"
     "net/http"
+    "os"
 )
 
 func main() {
+    host, exists := os.LookupEnv("SERVER_HOST")
+    if !exists {
+        host = ":5000"
+    }
     http.Handle("/", http.FileServer(http.Dir("/opt/www/static")))
-    log.Fatal(http.ListenAndServe(":5000", nil))
+    log.Fatal(http.ListenAndServe(host, nil))
 }
